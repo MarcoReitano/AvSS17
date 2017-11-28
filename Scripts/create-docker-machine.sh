52e97721a8a6d6a54eb436cfb7e265f643683196
Daniel Morady <dmorady@pip07.inf.FH-Koeln.DE>
2017-11-28 18:21:28 +0100

    Changed create-docker-machine to My Method.
    To Keep docker-machine ssh intact so i can use it for Building the
    Project for Testing. and its Working on all machines (exept 4) because i
    didn't added 4 back to the hosts file

--- Scripts/create-docker-machine.sh
+++ Scripts/create-docker-machine.sh
@@ -27,10 +27,10 @@ ssh -t -q "$USER@$ip" "PATH=$PATH;VBoxManage --version"
 ssh -t -q "$USER@$ip" "PATH=$PATH;docker-machine rm -f default"
 ##	# Hier  Können die Einstellungen für die Docker-Machine Verändert werden
 ssh -t -q "$USER@$ip" "PATH=$PATH;docker-machine create -d virtualbox --virtualbox-memory $memory --virtualbox-cpu-count $cpu_count --virtualbox-disk-size $disk_size default "
-ssh -t -q "$USER@$ip" "PATH=$PATH;echo \"/etc/init.d/services/dhcp stop;ifconfig eth0 10.0.0.$((100+i)) netmask 255.255.255.0 broadcast 10.0.0.255 up\" | docker-machine ssh default sudo tee /var/lib/boot2docker/bootsync.sh > /dev/null"
+ssh -t -q "$USER@$ip" "PATH=$PATH;echo \"/etc/init.d/services/dhcp stop;ifconfig eth2 10.0.0.$((100+i)) netmask 255.255.255.0 broadcast 10.0.0.255 up\" | docker-machine ssh default sudo tee /var/lib/boot2docker/bootsync.sh > /dev/null"
 ssh -t -q "$USER@$ip" "PATH=$PATH;docker-machine ssh default sudo chmod 755 /var/lib/boot2docker/bootsync.sh"
 ssh -t -q "$USER@$ip" "PATH=$PATH;docker-machine stop default"
-ssh -t -q "$USER@$ip" "PATH=$PATH;VBoxManage modifyvm 'default' --nic1 bridged --bridgeadapter1 en1 --cableconnected1 on"
-ssh -t -q "$USER@$ip" "PATH=$PATH;VBoxManage modifyvm 'default' --nic3 nat --cableconnected3 on"
+ssh -t -q "$USER@$ip" "PATH=$PATH;VBoxManage modifyvm 'default' --nic3 bridged --bridgeadapter3 en1 --cableconnected3 on"
+#ssh -t -q "$USER@$ip" "PATH=$PATH;VBoxManage modifyvm 'default' --nic3 nat --cableconnected3 on"
 echo $ip | xargs -n1 -P10 -I{}	ssh -t -q "$USER@$ip" "PATH=$PATH;docker-machine start default" &
 sleep 5
